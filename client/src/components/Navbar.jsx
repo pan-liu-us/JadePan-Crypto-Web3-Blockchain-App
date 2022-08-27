c55b2190388609fa728767c1f22c586593f85eef
Pan Liu <us.panliu@gmail.com>
2022-08-27 13:32:47 -0700

    refactor navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiMenu } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 import { FcDataEncryption } from "react-icons/fc";
@@ -8,7 +8,7 @@ const NavBarItem = ({ title }) => (
 );
 
 const Navbar = () => {
-  const [toggleMenu, setToggleMenu] = React.useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
@@ -24,23 +24,19 @@ const Navbar = () => {
         </li>
       </ul>
       <div className="flex relative">
-        {!toggleMenu && (
-          <HiMenu fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />
-        )}
-        {toggleMenu && (
-          <AiOutlineClose fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(false)} />
-        )}
-        {toggleMenu && (
-          <ul
-            className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-            flex flex-col justify-start items-end rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60 border-gray-200 text-white animate-slide-in"
-          >
-            <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
-            {["Market", "Exchange", "Tutorials", "Wallets"].map(
-              (item, index) => <NavBarItem key={item + index} title={item} />,
-            )}
-          </ul>
-        )}
+        {toggleMenu
+          ? <ul
+              className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+              flex flex-col justify-start items-end rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60 border-gray-200 text-white animate-slide-in"
+            >
+              <li className="text-xl w-full my-2">
+                <AiOutlineClose onClick={() => setToggleMenu(false)} />
+              </li>
+              {["Market", "Exchange", "Tutorials", "Wallets"].map(
+                (item, index) => <NavBarItem key={item + index} title={item} />,
+              )}
+            </ul>
+          : <HiMenu fontSize={28} className="text-white md:hidden cursor-pointer" onClick={() => setToggleMenu(true)} />}
       </div>
     </nav>
   );
