0c695709b288ad44db8454eca18a9580f3aa738b
Pan Liu <us.panliu@gmail.com>
2022-08-27 17:02:32 -0700

    add loader animation

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,7 +1,11 @@
-const Loader = () => {
-  return (
-    <h1>Loader</h1>
-  );
-};
+const Loader = () => (
+  <div className="flex justify-center items-center py-3">
+      <div class="progresser">
+        <div class="progresser__bar">
+          <div class="progresser__bar-item"></div>
+        </div>
+      </div>
+  </div>
+);
 
 export default Loader;
